92e25c832033e824f15644433d5ba1f316d93af4
Gene Merryman <gene.merryman@libertymutual.com>
2018-01-08 14:34:34 -0500

    initial commit

---
+++ src/main/java/com/lmig/gfc/roadgameraterbe/RoadGameRaterBeApplication.java
@@ -0,0 +1,12 @@
+package com.lmig.gfc.roadgameraterbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadGameRaterBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadGameRaterBeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lmig/gfc/roadgameraterbe/RoadGameRaterBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lmig.gfc.roadgameraterbe;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoadGameRaterBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
